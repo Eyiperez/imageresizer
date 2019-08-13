76bd06188d79a64d7811b6065f15c909de742da7
Erika Perez <erikaperez@Erikas-MacBook-Air.local>
2019-08-12 20:43:09 -0400

    added resize image function

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const app = express();
 const port = 3000;
 
 const SecurityService = require('./services/security');
+const ImageService = require('./services/image');
 
-app.get('/', (req, res, next) => {
+app.get('/', async (req, res, next) => {
     const {height, width, image} = req.query;
 
     try {
         //Check if all params are filled out
         SecurityService.checkForParams({height, width, image});
-        
+
+        //
+        const imageData = await ImageService.getImageDataFromUrl(image);
+        console.log(imageData)
+
         res.json({height, width, image})
     } catch(err) {
         next(err);
--- services/image.js
+++ services/image.js
@@ -1,12 +1,18 @@
 const axios = require('axios');
+cosnt sharp = require('sharp');
 const ImageService = {};
 
-ImageService.getImageDataFromUrl = (url) => {
+ImageService.getImageDataFromUrl = async (url) => {
     const response = await axios({
         method: 'get',
         url,
         responseType:'arraybuffer'
     });
+    return response.data;
+}
+
+ImageService.resizeImageData = async (imageData, height, width) => {
+   return await sharp(imageData).resize(height, width).png.toBuffer();
 }
 
 module.exports = ImageService;
